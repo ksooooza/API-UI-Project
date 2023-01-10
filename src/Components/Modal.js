80c6e1395ae6ad1fe21e0ac98351ecfd4364516e
ksooooza <ksoooza@gmail.com>
2023-01-10 15:09:30 -0500

    Switched to Poke API, got fetch request working

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import Poke from '../Poke.jsx'
 
 function Modal({ closeModal }) {
     return(
-        <div classNam="modalBackground">
+        <div className="modalBackground">
             <div className="modalContainer">
                 <button onClick={() => closeModal(false)}>X</button>
-                <div className="title"><h2>Name of fruit</h2></div>
-                <div className="body">Some facts about a fruit</div>
+                <div className="title"><h2>Name of Pokemon</h2></div>
+                <div className="body"><Poke /></div>
             </div>
         </div>
     )
